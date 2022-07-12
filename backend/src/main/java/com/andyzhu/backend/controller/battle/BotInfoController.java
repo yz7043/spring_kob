7f659402bcef1086a6c48e5d5920c72f2d8426fc
yz7043 <yz7043@nyu.edu>
2022-07-12 15:13:33 -0400

    cross origin header

--- backend/src/main/java/com/andyzhu/backend/controller/battle/BotInfoController.java
+++ backend/src/main/java/com/andyzhu/backend/controller/battle/BotInfoController.java
@@ -3,18 +3,19 @@ package com.andyzhu.backend.controller.battle;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/battle/")
 public class BotInfoController {
     @RequestMapping("getBotInfo/")
-    public List<String> getBotInfo(){
-        List<String> list = new LinkedList<>();
-        list.add("Haha");
-        list.add("hehe");
-        list.add("lala");
-        return list;
+    public Map<String, String> getBotInfo(){
+        Map<String, String> bot1 = new HashMap<>();
+        bot1.put("name", "sb");
+        bot1.put("rating", "1");
+        return bot1;
     }
 }
